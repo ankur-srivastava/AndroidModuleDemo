f46f6407badac0098b199160ba5e13d97d87d45f
Ankur Srivastava <ankurlkw@gmail.com>
2016-05-31 09:29:53 +0530

    Added Demo Unit Test

---
+++ app/src/testDemo/java/com/edocent/androidmoduledemo/DemoTest.java
@@ -0,0 +1,15 @@
+package com.edocent.androidmoduledemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by ankursrivastava on 5/31/16.
+ */
+public class DemoTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
